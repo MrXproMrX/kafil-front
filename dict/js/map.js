d52096c6fb5e042845a81388347c6858ce943f7c
MRXPRO200330020179Y <mrxpro2003@gmail.com>
2022-11-10 10:02:01 +0500

    узгаришлар

--- dict/js/map.js
+++ dict/js/map.js
@@ -1,17 +1,3 @@
-var i=0;
-	$(".contacts__menu__item a").each(function(){
-  length
-	i++;
-	$(this).attr("href","#map-"+i);
-});
-
-var i=0;
-	$(".contacts__map__item").each(function(){
-  length
-	i++;
-	$(this).attr("id","map-"+i);
-});
-
 
 // directions_in id end
 $('.contacts__map .contacts__map__item').hide();
@@ -24,26 +10,12 @@ $('.contacts__menu__item a').on('click', function(event){
   $('.contacts__menu__item li').removeClass('contacts__map__active');
   $(this).parent().addClass('contacts__map__active');
   $('.contacts__map .contacts__map__item').hide();
-  $($(this).attr('href')).show();
+  $($(this).attr('data-list')).show();
 });
 
 
 // ------------------------------------------------------------------
 
-
-var i=0;
-	$(".contacts__menu__list li").each(function(){
-  length
-	i++;
-	$(this).attr("href","#contacts-"+i);
-});
-
-var i=0;
-	$(".contacts__map__contacts__item").each(function(){
-  length
-	i++;
-	$(this).attr("id","contacts-"+i);
-});
 
 // directions_in id end
 $('.contacts__map__contacts .contacts__map__contacts__item').hide();
@@ -56,7 +28,7 @@ $('.contacts__menu__list li').on('click', function(event){
   $('.contacts__menu__item li').removeClass('contacts__map__contacts__active');
   $(this).parent().addClass('contacts__map__contacts__active');
   $('.contacts__map__contacts .contacts__map__contacts__item').hide();
-  $($(this).attr('href')).show();
+  $($(this).attr('data-item')).show();
 });
 
 // ------------------------------------------------------------------
